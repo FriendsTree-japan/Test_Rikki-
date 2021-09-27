2d298a27cede6fada40cee7ec93cff441a78b0d7
“RikitoKarita” <rktkrt12@gmail.com>
2021-09-28 00:00:08 +0900

    カルーセルスライダーの機能追加

--- lib/01_Select.dart
+++ lib/01_Select.dart
@@ -11,231 +11,191 @@ class Select extends StatefulWidget {
   _SelectState createState() => _SelectState();
 }
 
+
 class _SelectState extends State<Select> {
+  int _currentIndex = 0;
   final List<String> imgList = [
     'images/cat.jpeg',
     'images/cat2.jpeg',
     'images/cat3.jpeg',
-   ];
+  ];
+
+  int _currentIndex2 = 0;
+  final List<String> imgList2 = [
+    'images/cat.jpeg',
+    'images/cat2.jpeg',
+    'images/cat3.jpeg',
+  ];
+
+  int _currentIndex3 = 0;
+  final List<String> imgList3 = [
+    'images/cat.jpeg',
+    'images/cat2.jpeg',
+    'images/cat3.jpeg',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: SingleChildScrollView(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+
             children: <Widget>[
-              Text('昔ながらの' ,style: TextStyle(color: Colors.lightGreen, fontSize: 25, )),
-              Text('のプロフィール帳', style: TextStyle(fontSize: 20)),
-            ],
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                Column(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.center,
-                        child: RaisedButton(
-                            child: Image.asset('images/cat.jpeg', width: 370,),
-                            onPressed: () {
-                              Navigator.push(
-                                  context, MaterialPageRoute(builder: (context) => profile_001()));
-                            }
-                            ),
-                      ),
-                      Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                    ]
+
+              Text('昔ながらのプロフィール帳' ,style: TextStyle(color: Colors.black, fontSize: 20, )),
+              CarouselSlider(
+                options: CarouselOptions(
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                  autoPlay: true,
+                  onPageChanged: (index, reason) {
+                    setState(
+                          () {
+                        _currentIndex = index;
+                      },
+                    );
+                  },
                 ),
-                Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat2.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_002()));
-                          }
+                items: imgList.map((e) =>
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          Image.asset(e,
+                            width: 1050,
+                            height: 350,
+                            fit: BoxFit.cover,
+                          )
+                        ],
                       ),
-                    ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
+                    )).toList(),
               ),
 
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat3.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_003()));
-                          }
-                      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList.map((urlOfItem) {
+                  int index = imgList.indexOf(urlOfItem);
+                  return Container(
+                    width: 10.0,
+                    height: 10.0,
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentIndex == index
+                          ? Color.fromRGBO(0, 0, 0, 0.8)
+                          : Color.fromRGBO(0, 0, 0, 0.3),
                     ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
+                  );
+                }).toList(),
+              ),
+              Padding(padding: EdgeInsets.all(20.0)),
+
+              Text('ちょっと大人のプロフィール帳' ,style: TextStyle(color: Colors.black, fontSize: 20, )),
+              CarouselSlider(
+                options: CarouselOptions(
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                  autoPlay: true,
+                  onPageChanged: (index, reason) {
+                    setState(
+                          () {
+                        _currentIndex2 = index;
+                      },
+                    );
+                  },
+                ),
+                items: imgList2.map((e) =>
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          Image.asset(e,
+                            width: 1050,
+                            height: 350,
+                            fit: BoxFit.cover,)
+                        ],
+                      ),
+                    )).toList(),
               ),
 
-            ],
-          ),
-          ),
-          Padding(padding: EdgeInsets.all(10.0)),
-
-
-
-
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text('ちょっと大人の' ,style: TextStyle(color: Colors.lightGreen, fontSize: 25, )),
-              Text('のプロフィール帳', style: TextStyle(fontSize: 20)),
-            ],
-          ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: <Widget>[
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_004()));
-                          }
-                      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList2.map((urlOfItem) {
+                  int index = imgList2.indexOf(urlOfItem);
+                  return Container(
+                    width: 10.0,
+                    height: 10.0,
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentIndex2 == index
+                          ? Color.fromRGBO(0, 0, 0, 0.8)
+                          : Color.fromRGBO(0, 0, 0, 0.3),
                     ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
+                  );
+                }).toList(),
+              ),
+              Padding(padding: EdgeInsets.all(20.0)),
+              Text('推しメン・友人用のプロフィール帳' ,style: TextStyle(color: Colors.black, fontSize: 20, )),
+
+              CarouselSlider(
+                options: CarouselOptions(
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                  autoPlay: true,
+                  onPageChanged: (index, reason) {
+                    setState(
+                          () {
+                        _currentIndex3 = index;
+                      },
+                    );
+                  },
+                ),
+                items: imgList3.map((e) =>
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          Image.asset(e,
+                            width: 1050,
+                            height: 350,
+                            fit: BoxFit.cover,)
+                        ],
+                      ),
+                    )).toList(),
               ),
 
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat2.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_003()));
-                          }
-                      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList3.map((urlOfItem) {
+                  int index = imgList3.indexOf(urlOfItem);
+                  return Container(
+                    width: 10.0,
+                    height: 10.0,
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentIndex3 == index
+                          ? Color.fromRGBO(0, 0, 0, 0.8)
+                          : Color.fromRGBO(0, 0, 0, 0.3),
                     ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
-              ),
-
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat3.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_004()));
-                          }
-                      ),
-                    ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
-              ),
-
-            ],
-          ),
-          ),
-          Padding(padding: EdgeInsets.all(25.0)),
-
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text('推しメン用' ,style: TextStyle(color: Colors.lightGreen, fontSize: 25, )),
-              Text('のプロフィール帳', style: TextStyle(fontSize: 20)),
-            ],
-          ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: <Widget>[
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_001()));
-                          }
-                      ),
-                    ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
-              ),
-
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat2.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_001()));
-                          }
-                      ),
-                    ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
-              ),
-
-              Column(
-                  children: <Widget>[
-
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                          child: Image.asset('images/cat3.jpeg', width: 370,),
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => profile_001()));
-                          }
-                      ),
-                    ),
-                    Text('作成者：YusukeArai' ,style: TextStyle(color: Colors.black12, fontSize: 12, )),
-                  ]
+                  );
+                }).toList(),
               ),
 
             ],
           ),
           ),
-          Padding(padding: EdgeInsets.all(10.0)),
-
-
-        ],
-      ),
         ),
-
+      ),
     );
   }
 }
